612f9be47ce43ca21559722704ae46500141ba29
Jarrod Seccombe <jsec@hey.com>
2021-08-17 22:28:06 -0400

    [nvim] show lsp source in diagnostics messages

--- neovim/.config/nvim/lua/_lsp/handlers/diagnostics.lua
+++ neovim/.config/nvim/lua/_lsp/handlers/diagnostics.lua
@@ -1,12 +1,36 @@
 local M = {}
 
 function M.on_attach()
-    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-    })
+    vim.lsp.handlers["textDocument/publishDiagnostics"] =
+        function(_, _, params, client_id, _)
+            local config = {
+                underline = true,
+                virtual_text = false,
+                signs = true,
+                update_in_insert = false,
+            }
+
+            local uri = params.uri
+            local bufnr = vim.uri_to_bufnr(uri)
+
+            if not bufnr then
+              return
+            end
+
+            local diagnostics = params.diagnostics
+
+            for i, v in ipairs(diagnostics) do
+                diagnostics[i].message = string.format("%s: %s", v.source, v.message)
+            end
+
+            vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
+
+            if not vim.api.nvim_buf_is_loaded(bufnr) then
+                return
+            end
+
+            vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
+        end
 
     vim.o.updatetime = 250
     vim.cmd('autocmd CursorHold,CursorHoldI * lua vim.lsp.diagnostic.show_line_diagnostics({focusable=false, show_header=false, border="double"})')
