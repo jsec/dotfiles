3edd929e465315688d7baca0068d976217d9971d
Jarrod Seccombe <jsec@hey.com>
2021-10-08 22:50:16 -0400

    [nvim] switch colorscheme again because ADD

--- neovim/.config/nvim/lua/_statusbar/init.lua
+++ neovim/.config/nvim/lua/_statusbar/init.lua
@@ -1,6 +1,6 @@
 require('lualine').setup {
   options = {
-    theme = 'tokyonight'
+    theme = 'github'
   },
   sections = {
     lualine_a = { 'mode' },
--- neovim/.config/nvim/lua/colorscheme.lua
+++ neovim/.config/nvim/lua/colorscheme.lua
@@ -2,15 +2,17 @@ vim.cmd('set termguicolors')
 vim.cmd('syntax enable')
 vim.cmd('set background=dark')
 
-vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_italic_comments = 1
-vim.g.tokyonight_italic_keywords = 1
-vim.g.tokyonight_sidebars = { 'packer', 'nvim-tree', 'terminal' }
-vim.g.tokyonight_dark_sidebar = 1
-vim.g.tokyonight_dark_float = 1
-
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.cmd[[colorscheme tokyonight]]
+require('github-theme').setup({
+  theme_style = 'dimmed',
+  function_style = 'bold',
+  keyword_style = 'NONE',
+  comment_style = 'italic',
+  dark_sidebar = true,
+  colors = {
+    hint = "orange",
+    error = "#ff0000",
+  }
+})
 
 vim.cmd('hi EndOfBuffer ctermfg=white')
 vim.cmd('highlight MatchParen ctermfg=white ctermbg=black')
