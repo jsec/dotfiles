1c9ef53a9dae88af0a1f2ad1585fdab2df46a38a
Jarrod Seccombe <jsec@hey.com>
2021-11-30 20:09:21 -0500

    [nvim] improve completion menu UI

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -14,7 +14,6 @@ return require('packer').startup(function()
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     use { 'nvim-treesitter/playground' }
     use { 'folke/tokyonight.nvim' }
-    use { 'projekt0n/github-nvim-theme' }
     use { 'b3nj5m1n/kommentary' }
     use {
       'phaazon/hop.nvim',
