0089c1b3c1911ed3ea0aacd102fa2bfdb3de0c69
Jarrod Seccombe <jsec@hey.com>
2021-11-30 20:50:20 -0500

    [nvim] update to neovim 0.6

--- neovim/.config/nvim/lua/_lsp/appearance/diagnostics.lua
+++ neovim/.config/nvim/lua/_lsp/appearance/diagnostics.lua
@@ -4,7 +4,7 @@ function M.setup()
     local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
     for type, icon in pairs(signs) do
-      local hl = "LspDiagnosticsSign" .. type
+      local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
 end
--- neovim/.config/nvim/lua/_lsp/handlers/diagnostics.lua
+++ neovim/.config/nvim/lua/_lsp/handlers/diagnostics.lua
@@ -1,38 +1,11 @@
 local M = {}
 
 function M.on_attach()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] =
-        function(_, result, ctx, _)
-            local config = {
-                underline = true,
-                virtual_text = true,
-                signs = true,
-                update_in_insert = false,
-            }
-
-            local uri = result.uri
-            local bufnr = vim.uri_to_bufnr(uri)
-
-            if not bufnr then
-              return
-            end
-
-            local diagnostics = result.diagnostics
-
-            for i, v in ipairs(diagnostics) do
-                diagnostics[i].message = string.format("%s: %s", v.source, v.message)
-            end
-
-            vim.lsp.diagnostic.save(diagnostics, bufnr, ctx.client_id)
-
-            if not vim.api.nvim_buf_is_loaded(bufnr) then
-                return
-            end
-
-            vim.lsp.diagnostic.display(diagnostics, bufnr, ctx.client_id, config)
-        end
-
-    vim.o.updatetime = 250
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = {
+      source = "always"
+    }
+  })
 end
 
 return M
