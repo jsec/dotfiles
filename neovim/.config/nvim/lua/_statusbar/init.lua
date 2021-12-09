9b317817f6d2bb0c91bacfe9c689d05dbd41816a
Jarrod Seccombe <jsec@hey.com>
2021-12-08 20:58:48 -0500

    [nvim] update statusline

--- neovim/.config/nvim/lua/_statusbar/init.lua
+++ neovim/.config/nvim/lua/_statusbar/init.lua
@@ -1,23 +1,68 @@
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
+
+-- stylua: ignore
+local colors = {
+  blue   = '#7aa2f7',
+  cyan   = '#7dcfff',
+  white  = '#c0caf5',
+  red    = '#f7768e',
+  yellow = '#e0af68',
+  grey   = '#414868',
+  black  = '#1f2335',
+  green  = '#9ece6a',
+  muted  = '#565f89'
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.blue },
+    b = { fg = colors.blue, bg = colors.grey },
+    c = { fg = colors.black, bg = colors.black },
+  },
+
+  insert = {
+    a = { fg = colors.black, bg = colors.green },
+    z = { fg = colors.black, bg = colors.green },
+  },
+
+  visual = { a = { fg = colors.black, bg = colors.yellow } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.muted, bg = colors.black },
+    b = { fg = colors.muted, bg = colors.black },
+    c = { fg = colors.muted, bg = colors.black },
+  },
+}
+
 require('lualine').setup {
   options = {
-    theme = 'tokyonight'
+    theme = bubbles_theme,
+    component_separators = '|',
+    section_separators = { left = '', right = '' },
   },
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff' },
-    lualine_c = { 'filename' },
-    lualine_x = { { "diagnostics", sources = { "nvim_lsp" } } },
-    lualine_y = { 'encoding', 'fileformat', 'filetype' },
-    lualine_z = { 'location' }
+    lualine_a = {
+      { 'mode', separator = { left = '' }, right_padding = 2 },
+    },
+    lualine_b = { 'filename', 'diff' },
+    lualine_c = { { 'diagnostics', sources = { 'nvim_lsp' } } },
+    lualine_x = {},
+    lualine_y = { 'filetype', 'encoding' },
+    lualine_z = {
+      { 'branch', separator = { right = '' }, left_padding = 2 },
+    },
   },
   inactive_sections = {
-    lualine_a = {},
-    lualine_b = {'filename'},
-    lualine_c = {'filetype'},
-    lualine_x = {'location'},
+    lualine_a = { 'filename' },
+    lualine_b = { { 'diagnostics', sources = { 'nvim_lsp' } } },
+    lualine_c = {},
+    lualine_x = {},
     lualine_y = {},
-    lualine_z = {}
+    lualine_z = { 'branch' },
   },
   tabline = {},
-  extensions = { 'nvim-tree' }
+  extensions = {},
 }
