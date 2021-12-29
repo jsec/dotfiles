94fd4549e99b2d2f644e987c84347b3805e3861a
Jarrod Seccombe <jarrod.seccombe@cpspharm.com>
2021-12-29 13:07:48 -0500

    [nvim] more lualine cleanup

--- neovim/.config/nvim/lua/_statusbar/init.lua
+++ neovim/.config/nvim/lua/_statusbar/init.lua
@@ -1,8 +1,3 @@
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
--- stylua: ignore
 local colors = {
   blue        = '#7aa2f7',
   white       = '#c0caf5',
