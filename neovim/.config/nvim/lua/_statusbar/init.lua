c28272e3691777972e1973409154bf1c34c431d0
Jarrod Seccombe <jarrod.seccombe@cpspharm.com>
2021-12-29 12:31:40 -0500

    [nvim] update lualine

--- neovim/.config/nvim/lua/_statusbar/init.lua
+++ neovim/.config/nvim/lua/_statusbar/init.lua
@@ -4,65 +4,131 @@
 
 -- stylua: ignore
 local colors = {
-  blue   = '#7aa2f7',
-  cyan   = '#7dcfff',
-  white  = '#c0caf5',
-  red    = '#f7768e',
-  yellow = '#e0af68',
-  grey   = '#414868',
-  black  = '#1f2335',
-  green  = '#9ece6a',
-  muted  = '#565f89'
+  blue        = '#7aa2f7',
+  white       = '#c0caf5',
+  red         = '#f7768e',
+  orange      = '#e0af68',
+  grey        = '#414868',
+  black       = '#1f2335',
+  light_green = '#9ece6a',
 }
 
-local bubbles_theme = {
+local theme = {
   normal = {
     a = { fg = colors.black, bg = colors.blue },
     b = { fg = colors.blue, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
+    c = { fg = colors.blue, bg = colors.black },
+    z = { fg = colors.blue, bg = colors.black },
   },
-
-  insert = {
-    a = { fg = colors.black, bg = colors.green },
-    z = { fg = colors.black, bg = colors.green },
-  },
-
-  visual = { a = { fg = colors.black, bg = colors.yellow } },
-  replace = { a = { fg = colors.black, bg = colors.red } },
-
-  inactive = {
-    a = { fg = colors.muted, bg = colors.black },
-    b = { fg = colors.muted, bg = colors.black },
-    c = { fg = colors.muted, bg = colors.black },
-  },
+  insert = { a = { fg = colors.black, bg = colors.light_green } },
+  visual = { a = { fg = colors.black, bg = colors.orange } },
+  replace = { a = { fg = colors.black, bg = colors.green } },
 }
+
+local empty = require('lualine.component'):extend()
+function empty:draw(default_highlight)
+  self.status = ''
+  self.applied_separator = ''
+  self:apply_highlights(default_highlight)
+  self:apply_section_separators()
+  return self.status
+end
+
+-- Put proper separators and gaps between components in sections
+local function process_sections(sections)
+  for name, section in pairs(sections) do
+    local left = name:sub(9, 10) < 'x'
+    for pos = 1, name ~= 'lualine_z' and #section or #section - 1 do
+      table.insert(section, pos * 2, { empty, color = { fg = colors.black, bg = colors.black } })
+    end
+    for id, comp in ipairs(section) do
+      if type(comp) ~= 'table' then
+        comp = { comp }
+        section[id] = comp
+      end
+      comp.separator = left and { right = '' } or { left = '' }
+    end
+  end
+  return sections
+end
+
+local function search_result()
+  if vim.v.hlsearch == 0 then
+    return ''
+  end
+  local last_search = vim.fn.getreg '/'
+  if not last_search or last_search == '' then
+    return ''
+  end
+  local searchcount = vim.fn.searchcount { maxcount = 9999 }
+  return last_search .. '(' .. searchcount.current .. '/' .. searchcount.total .. ')'
+end
+
+local function modified()
+  if vim.bo.modified then
+    return '+'
+  elseif vim.bo.modifiable == false or vim.bo.readonly == true then
+    return '-'
+  end
+  return ''
+end
 
 require('lualine').setup {
   options = {
-    theme = bubbles_theme,
-    component_separators = '|',
-    section_separators = { left = '', right = '' },
+    theme = theme,
+    component_separators = '',
+    section_separators = { left = '', right = '' },
   },
-  sections = {
-    lualine_a = {
-      { 'mode', separator = { left = '' }, right_padding = 2 },
+  sections = process_sections {
+    lualine_a = { 'mode' },
+    lualine_b = {
+      'branch',
+      { 'filename', file_status = false, path = 1 },
+      {
+        'diagnostics',
+        source = { 'nvim' },
+        sections = { 'error' },
+        diagnostics_color = { error = { bg = colors.red, fg = colors.black } },
+      },
+      {
+        'diagnostics',
+        source = { 'nvim' },
+        sections = { 'warn' },
+        diagnostics_color = { warn = { bg = colors.orange, fg = colors.black } },
+      },
+      {
+        'diagnostics',
+        source = { 'nvim' },
+        sections = { 'hint' },
+        diagnostics_color = { warn = { bg = colors.light_green, fg = colors.black } },
+      },
+      { modified, color = { bg = colors.white, fg = colors.black } },
+      {
+        '%w',
+        cond = function()
+          return vim.wo.previewwindow
+        end,
+      },
+      {
+        '%r',
+        cond = function()
+          return vim.bo.readonly
+        end,
+      },
+      {
+        '%q',
+        cond = function()
+          return vim.bo.buftype == 'quickfix'
+        end,
+      },
     },
-    lualine_b = { 'filename', 'diff' },
-    lualine_c = { { 'diagnostics', sources = { 'nvim_diagnostic' } } },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'encoding' },
-    lualine_z = {
-      { 'branch', separator = { right = '' }, left_padding = 2 },
-    },
+    lualine_c = {},
+    lualine_x = { 'diff' },
+    lualine_y = { search_result, 'filetype' },
+    lualine_z = { '%l:%c', '%p%%/%L' },
   },
   inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = { { 'diagnostics', sources = { 'nvim_diagnostic' } } },
-    lualine_c = {},
+    lualine_c = { '%f %y %m' },
     lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'branch' },
   },
-  tabline = {},
-  extensions = {},
 }
