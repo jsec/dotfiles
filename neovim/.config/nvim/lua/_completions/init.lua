52a05bac6b274f0bdef67e4caec9706309015b7d
Jarrod Seccombe <jarrod.seccombe@cpspharm.com>
2022-05-09 11:42:55 -0400

    [nvim] fix nvim-cmp deprecated mappings

--- neovim/.config/nvim/lua/_completions/init.lua
+++ neovim/.config/nvim/lua/_completions/init.lua
@@ -29,13 +29,13 @@ cmp.setup({
       require('luasnip').lsp_expand(args.body)
     end
   },
-  mapping = {
+  mapping = cmp.mapping.preset.insert({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true })
-  },
+  }),
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
