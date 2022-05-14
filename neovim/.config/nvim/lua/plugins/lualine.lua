b330b7eab57af6a08c3a06634d0cd6b83ca0eb0c
Jarrod Seccombe <jsec@hey.com>
2022-05-14 15:10:58 -0400

    [nvim] update lualine to use new colors util

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -2,23 +2,7 @@
 -- Author: jsec
 -- Credit: shadmansaleh && glepnir
 local lualine = require('lualine')
-
--- Color table for highlights
--- stylua: ignore
-local colors = {
-  bg        = '#262626',
-  fg        = '#bbc2cf',
-  yellow    = '#ffaf00',
-  cyan      = '#008080',
-  darkblue  = '#081633',
-  green     = '#619955',
-  orange    = '#FF8800',
-  violet    = '#a9a1e1',
-  magenta   = '#C586C0',
-  blue      = '#0a7aca',
-  red       = '#f44747',
-  lightblue = '#5CB6F8'
-}
+local colors = require('utils.colors')
 
 local mode_color = {
   n = colors.red,
