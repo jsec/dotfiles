b34a9b4b503af9d7487968cb0ca4cc2a4cd1b759
Jarrod Seccombe <jarrod.seccombe@cpspharm.com>
2022-11-09 10:15:08 -0500

    [nvim] update nvim-cmp-lsp capabilities

--- neovim/.config/nvim/lua/_lsp/init.lua
+++ neovim/.config/nvim/lua/_lsp/init.lua
@@ -6,7 +6,7 @@ local lsp_format = require('lsp-format')
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 lsp_format.setup {
   javascript = {
