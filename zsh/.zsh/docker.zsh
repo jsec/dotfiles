afc35b7ad41a906c8aa306d6b76301f0f9ba2c7e
Jarrod Seccombe <jarrod.seccombe@cpspharm.com>
2022-12-07 15:35:47 -0500

    [zsh] add more docker aliases

--- zsh/.zsh/docker.zsh
+++ zsh/.zsh/docker.zsh
@@ -1,3 +1,7 @@
 # Compose
 alias dcdown='docker compose -f docker-compose.dev.yml down'
 alias dcup='docker compose -f docker-compose.dev.yml up -d'
+alias dprune='docker system prune'
+alias dkill='docker stop $(docker ps -aq) && docker rm $(docker ps -aq)'
+alias dpurge='docker rmi $(docker images -q)'
+alias dnuke='dkill && dpurge'
