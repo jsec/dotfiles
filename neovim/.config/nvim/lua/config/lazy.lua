fe96c4761dd615f2fe34e2a8f1f77cfb428cf658
Jarrod Seccombe <jarrod.seccombe@gmail.com>
2023-01-28 15:00:57 -0500

    bootstrap lazy and add colorscheme

---
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+function M.check_install()
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+    if not vim.loop.fs_stat(lazypath) then
+      vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+      })
+    end
+
+    vim.opt.rtp:prepend(lazypath)
+end
+
+function M.init()
+    M.check_install()
+    require('config.options')
+    require('lazy').setup('plugins-new')
+end
+
+return M
---
+++ neovim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = ','
+
+if vim.fn.has('termguicolors') then
+    vim.cmd('set termguicolors')
+end
