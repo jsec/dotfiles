9ffcc589d773b2137f747dfd3af4b5bd39fe63f7
Jarrod Seccombe <jarrod.seccombe@gmail.com>
2023-01-28 15:45:29 -0500

    convert options file

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,53 @@
 vim.g.mapleader = ','
 
-if vim.fn.has('termguicolors') then
-    vim.cmd('set termguicolors')
-end
+vim.opt.autoread = true
+vim.opt.wildmenu = true
+vim.opt.laststatus = 2
+vim.opt.encoding = 'utf-8'
+vim.opt.laststatus = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.wrap = true
+vim.opt.smartcase = true
+vim.opt.autoindent = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.splitright = true
+vim.opt.cmdheight = 1
+vim.opt.updatetime = 300
+vim.opt.termguicolors = true
+vim.opt.completeopt = { 'menuone', 'noselect' }
+vim.opt.scrolloff = 10
+vim.opt.mouse = 'a'
+vim.opt.visualbell = false
+vim.opt.nu = true
+vim.opt.wildignore = {
+    '**/node_modules',
+    '**/tmp',
+    '**/dist',
+    '**/.git',
+    '**/bin'
+}
+
+-- State directories
+local cache_dir = vim.env.HOME .. '/.cache/nvim/'
+
+vim.opt.backup = true
+vim.opt.backupdir = cache_dir .. 'backup/'
+vim.opt.directory = cache_dir .. 'swap/'
+vim.opt.undofile = true
+vim.opt.undodir = cache_dir .. 'undo/'
+
+-- Filetype settings
+vim.cmd('autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll')
+
+-- Trim whitespace
+-- vim.g.better_whitespace_enabled=1
+-- vim.g.strip_whitespace_on_save = 1
+-- vim.g.strip_whitespace_confirm = 0
+
+-- Python hosts
+-- vim.g.python2_host_prog = "/usr/local/bin/python2"
+-- vim.g.python3_host_prog = "/usr/local/bin/python3"
