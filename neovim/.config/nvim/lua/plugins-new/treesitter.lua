841867db35cd0a7fe3ae8e74e6e3a509adfc58e5
Jarrod Seccombe <jarrod.seccombe@gmail.com>
2023-01-29 19:22:21 -0500

    fix treesitter setup

--- neovim/.config/nvim/lua/plugins-new/treesitter.lua
+++ neovim/.config/nvim/lua/plugins-new/treesitter.lua
@@ -2,53 +2,55 @@ return {
     { 'nvim-treesitter/playground', cmd = 'TSPlaygroundToggle' },
     {
         'nvim-treesitter/nvim-treesitter',
-        opts = {
-            ensure_installed = {
-                'bash',
-                'c',
-                'cmake',
-                'cpp',
-                'css',
-                'diff',
-                'gitignore',
-                'go',
-                'graphql',
-                'help',
-                'html',
-                'http',
-                'javascript',
-                'jsdoc',
-                'json',
-                'jsonc',
-                'lua',
-                'markdown',
-                'markdown_inline',
-                'python',
-                'query',
-                'regex',
-                'rust',
-                'scss',
-                'sql',
-                'toml',
-                'tsx',
-                'typescript',
-                'vim',
-                'vue',
-                'yaml'
-            },
-            highlight = { enable = true },
-            query_linter = {
-                enable = true,
-                use_virtual_text = true,
-                lint_events = { 'BufWrite', 'CursorHold' }
-            },
-            playground = {
-                enable = true,
-                disable = {},
-                updatetime = 25,
-                persist_queries = true
-            },
-            additional_vim_regex_highlighting = false
-        }
+        config = function()
+            require('nvim-treesitter.configs').setup({
+                ensure_installed = {
+                    'bash',
+                    'c',
+                    'cmake',
+                    'cpp',
+                    'css',
+                    'diff',
+                    'gitignore',
+                    'go',
+                    'graphql',
+                    'help',
+                    'html',
+                    'http',
+                    'javascript',
+                    'jsdoc',
+                    'json',
+                    'jsonc',
+                    'lua',
+                    'markdown',
+                    'markdown_inline',
+                    'python',
+                    'query',
+                    'regex',
+                    'rust',
+                    'scss',
+                    'sql',
+                    'toml',
+                    'tsx',
+                    'typescript',
+                    'vim',
+                    'vue',
+                    'yaml'
+                },
+                highlight = { enable = true },
+                query_linter = {
+                    enable = true,
+                    use_virtual_text = true,
+                    lint_events = { 'BufWrite', 'CursorHold' }
+                },
+                playground = {
+                    enable = true,
+                    disable = {},
+                    updatetime = 25,
+                    persist_queries = true
+                },
+                additional_vim_regex_highlighting = false
+            })
+        end
     }
 }
