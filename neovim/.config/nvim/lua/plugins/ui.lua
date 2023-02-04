b4031e169d44703e6d4961aa0e6778a4236f0038
Jarrod Seccombe <jarrod.seccombe@gmail.com>
2023-02-04 01:08:50 -0500

    [nvim] add web-devicons dependency to nvim-tree

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -18,6 +18,9 @@ return {
     },
     {
         'nvim-tree/nvim-tree.lua',
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
         keys = {
             { '<c-n>', '<cmd>NvimTreeToggle<cr>', desc = 'NvimTree' },
         },
