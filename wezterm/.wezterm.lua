f08ceb85626f29cf6e5d3aab03c0de0699eed3f7
Jarrod Seccombe <Jarrod.Seccombe@icloud.com>
2023-04-09 07:52:49 -0400

    [wezterm] add new wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require('wezterm')
+
+local config = {}
+
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.color_scheme = 'nord'
+
+config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
+config.font_size = 13.0
+config.front_end = 'WebGpu'
+config.enable_tab_bar = false
+
+return config
