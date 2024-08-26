0438941a85b9f88423768a0465d0680fd6f8dab1
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-08-25 23:13:00 -0400

    [wezterm] update config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,8 +10,9 @@ config.color_scheme = 'Catppuccin Mocha'
 
 config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
 config.font_size = 13.0
---[[ config.font = wezterm.font('MonoLisa Custom', { weight = 'Medium' })
-config.font_size = 12.5 ]]
+--[[ config.font = wezterm.font('CommitMono', { weight = 'Regular' })
+config.font_size = 13.0
+config.line_height = 1.2 ]]
 config.front_end = 'WebGpu'
 config.enable_tab_bar = false
 
