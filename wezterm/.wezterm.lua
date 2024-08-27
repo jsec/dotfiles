769d14bde19daf62851e709af32a71585db2837f
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-08-27 13:48:38 -0400

    [wezterm] enable native macos fullscreen

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -12,5 +12,6 @@ config.font = wezterm.font('JetBrains Mono', { weight = 'Medium' })
 config.font_size = 13.0
 config.front_end = 'WebGpu'
 config.enable_tab_bar = false
+config.native_macos_fullscreen_mode = true
 
 return config
