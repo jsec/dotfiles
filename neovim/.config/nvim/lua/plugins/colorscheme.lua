8c3b11cd55d9a9e6c066a060f11c0cddfd417c4d
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2024-11-22 13:06:54 -0500

    [nvim] sync plugins

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -55,29 +55,5 @@ return {
                 }
           })
       end
-  },
-  {
-      'projekt0n/github-nvim-theme',
-      name = 'github-theme',
-      lazy = false,
-      priority = 1000,
-      config = function()
-          require('github-theme').setup({
-              options = {
-                  styles = {
-                      comments = 'italic',
-                      keywords = 'italic',
-                      functions = 'italic',
-                      conditionals = 'italic'
-                  },
-                  darken = {
-                      floats = true,
-                      sidebars = {
-                          enable = true
-                      }
-                  }
-              }
-          })
-      end
   }
 }
