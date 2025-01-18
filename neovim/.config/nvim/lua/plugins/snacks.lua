115c1bc7687085e14018fd748f0cc259047a881e
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-01-18 12:22:09 -0500

    [nvim] start telescope->snacks.picker transition

--- neovim/.config/nvim/lua/plugins/snacks.lua
+++ neovim/.config/nvim/lua/plugins/snacks.lua
@@ -15,6 +15,62 @@ return {
             }
           }
         }
+      },
+      picker = {
+        matcher = {
+          fuzzy = true,
+          smartcase = true,
+          ignorecase = true,
+          sort_empty = false,
+          filename_bonus = true
+        },
+        sort = {
+          fields = { 'score:desc', '#text', 'idx' }
+        },
+        formatters = {
+          file = {
+            filename_first = true
+          },
+          selected = {
+            show_always = false,
+            unselected = true
+          }
+        },
+        win = {
+          input = {
+            keys = {
+              ["<Esc>"] = { "close", mode = { "n", "i" } },
+            }
+          }
+        },
+      }
+    },
+    keys = {
+      { 
+        '<c-p>', 
+        function() 
+          if vim.b.gitsigns_head or vim.b.gitsigns_status_dict then
+            Snacks.picker.git_files({ untracked = true })
+          else 
+            Snacks.picker.files()
+          end
+        end, 
+        desc = "Find Files" 
+      },
+      { 
+        "<leader>gd", 
+        function() 
+          Snacks.picker.lsp_definitions({ confirm = 'edit_vsplit' }) 
+        end, 
+        desc = "Goto Definition" 
+      },
+      { 
+        "<leader>gr", 
+        function() 
+          Snacks.picker.lsp_references({ confirm = 'edit_vsplit' }) 
+        end, 
+        nowait = true, 
+        desc = "Find References" 
       }
     }
   }
