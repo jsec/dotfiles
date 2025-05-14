b0c6a52dfe03201f656bac58f67e791b002613e0
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-05-13 22:48:48 -0400

    [nvim] fix diagnostic deprecations for 0.11

--- neovim/.config/nvim/lua/util/lsp.lua
+++ neovim/.config/nvim/lua/util/lsp.lua
@@ -1,26 +1,44 @@
 local M = {}
 
-local set_diagnostic_signs = function()
-  local signs = { Error = ' ', Warn = ' ', Hint = ' ', Information = ' ' }
-
-  for type, icon in pairs(signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
-  end
-end
-
-local set_handlers = function()
-  vim.lsp.handlers['textDocument/publishDiagnostics'] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = {
-        source = 'always',
-      },
+local set_diagnostics = function()
+    vim.diagnostic.config({
+        virtual_text = true,
+        underline = true,
+        signs = {
+            text = {
+                [vim.diagnostic.severity.ERROR] = '',
+                [vim.diagnostic.severity.WARN] = '',
+                [vim.diagnostic.severity.INFO] = ' ',
+                [vim.diagnostic.severity.HINT] = ' ',
+            },
+            linehl = {
+                [vim.diagnostic.severity.ERROR] = '',
+                [vim.diagnostic.severity.WARN] = '',
+                [vim.diagnostic.severity.INFO] = '',
+                [vim.diagnostic.severity.HINT] = '',
+            },
+            numhl = {
+                [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+                [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+                [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+                [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+            },
+        },
     })
 end
 
+local set_handlers = function()
+    vim.lsp.handlers['textDocument/publishDiagnostics'] =
+        vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = {
+                source = 'always',
+            },
+        })
+end
+
 M.on_attach = function(client, bufnr)
-  set_diagnostic_signs()
-  set_handlers()
+    set_diagnostics()
+    set_handlers()
 end
 
 return M
