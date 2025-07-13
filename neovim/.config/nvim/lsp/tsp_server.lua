242f3769f7c8bc48a1bf25481f178cf043eef8a8
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-07-13 18:15:34 -0400

    [nvim] remove util folder and convert remaining lspconfig servers to native lsp support

---
+++ neovim/.config/nvim/lsp/tsp_server.lua
@@ -0,0 +1,6 @@
+---@type vim.lsp.Config
+return {
+    cmd = { 'tsp-server', '--stdio' },
+    filetypes = { 'typespec' },
+    root_markers = { 'tspconfig.yaml', '.git' },
+}
--- neovim/.config/nvim/lua/core/lsp.lua
+++ neovim/.config/nvim/lua/core/lsp.lua
@@ -1,13 +1,48 @@
 vim.lsp.config('*', {
     capabilities = require('cmp_nvim_lsp').default_capabilities(),
-    on_attach = require('util.lsp').on_attach
 })
 
-vim.lsp.enable({
-    'vtsls',
-    'eslint',
-    'ruff',
-    'basedpyright',
-    'gopls',
-    'jsonls',
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = {
+            source = 'always',
+        },
+    }
+)
+
+vim.diagnostic.config({
+    virtual_text = true,
+    underline = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = ' ',
+            [vim.diagnostic.severity.HINT] = ' ',
+        },
+        linehl = {
+            [vim.diagnostic.severity.ERROR] = '',
+            [vim.diagnostic.severity.WARN] = '',
+            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.HINT] = '',
+        },
+        numhl = {
+            [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+            [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+            [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+            [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+        },
+    },
+})
+
+vim.api.nvim_create_autocmd({ 'BufReadPre', 'BufNewFile' }, {
+    once = true,
+    callback = function()
+        local server_configs = vim.iter(vim.api.nvim_get_runtime_file('lsp/*.lua', true))
+            :map(function(file)
+                return vim.fn.fnamemodify(file, ':t:r')
+            end)
+            :totable()
+        vim.lsp.enable(server_configs)
+    end,
 })
