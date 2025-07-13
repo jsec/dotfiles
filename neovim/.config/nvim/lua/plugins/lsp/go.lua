227eac90209be2d72d36eb06d45e5fd335a30789
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-07-13 18:23:35 -0400

    [nvim] remove nvim-lspconfig plugin

--- neovim/.config/nvim/lua/plugins/lsp/go.lua
+++ neovim/.config/nvim/lua/plugins/lsp/go.lua
@@ -1,14 +1,13 @@
 return {
-  'ray-x/go.nvim',
-  dependencies = {
-    'ray-x/guihua.lua',
-    'neovim/nvim-lspconfig',
-    'nvim-treesitter/nvim-treesitter',
-  },
-  ft = { 'go', 'gomod' },
-  event = { 'CmdLineEnter' },
-  build = ':lua require("go.install").update_all_sync()',
-  config = function()
-    require('go').setup()
-  end,
+    'ray-x/go.nvim',
+    dependencies = {
+        'ray-x/guihua.lua',
+        'nvim-treesitter/nvim-treesitter',
+    },
+    ft = { 'go', 'gomod' },
+    event = { 'CmdLineEnter' },
+    build = ':lua require("go.install").update_all_sync()',
+    config = function()
+        require('go').setup()
+    end,
 }
