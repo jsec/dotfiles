9af248f0d6db73e9f5c6bfcae9d77385de1a4b78
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-11-01 18:26:24 -0400

    [nvim] update deprecated leap.add_default_mappings() call

--- neovim/.config/nvim/lua/plugins/leap.lua
+++ neovim/.config/nvim/lua/plugins/leap.lua
@@ -1,6 +1,7 @@
 return {
-  'ggandor/leap.nvim',
-  config = function()
-    require('leap').add_default_mappings()
-  end,
+    'ggandor/leap.nvim',
+    config = function()
+        vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+        vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
+    end,
 }
