4972293caddd5331806d23bbbf062012f697013e
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-11-19 14:13:33 -0500

    [nvim] remove deprecated vim.lsp.with command

--- neovim/.config/nvim/lua/core/lsp.lua
+++ neovim/.config/nvim/lua/core/lsp.lua
@@ -2,13 +2,6 @@ vim.lsp.config('*', {
     capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
-vim.lsp.handlers['textDocument/publishDiagnostics'] =
-    vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = {
-            source = 'always',
-        },
-    })
-
 vim.diagnostic.config({
     virtual_text = true,
     underline = true,
