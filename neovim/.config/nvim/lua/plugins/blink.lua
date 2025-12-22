c784e660d40273f378eab25584b970f23a309d31
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2025-12-21 19:57:54 -0500

    better blink integration

--- neovim/.config/nvim/lua/plugins/blink.lua
+++ neovim/.config/nvim/lua/plugins/blink.lua
@@ -1,20 +1,82 @@
 return {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
     version = '1.*',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = { preset = 'enter' },
-        appearance = {
-            nerd_font_variant = 'mono'
-        },
-        completion = { documentation = { auto_show = true } },
-        sources = {
-            default = { 'lsp', 'path', 'buffer', 'snippets' },
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" }
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'onsails/lspkind.nvim'
     },
-    opts_extend = { "sources.default" }
+    config = function()
+        require('lspkind').init({
+            mode = 'symbol_text',
+            symbol_map = {
+                Array = " ",
+                Boolean = " ",
+                Class = " ",
+                Color = " ",
+                Constant = " ",
+                Constructor = " ",
+                Enum = " ",
+                EnumMember = " ",
+                Event = " ",
+                Field = " ",
+                File = " ",
+                Folder = "󰉋 ",
+                Function = " ",
+                Interface = " ",
+                Key = " ",
+                Keyword = " ",
+                Method = " ",
+                Module = " ",
+                Namespace = " ",
+                Null = "󰟢",
+                Number = " ",
+                Object = " ",
+                Operator = " ",
+                Package = " ",
+                Property = " ",
+                Reference = " ",
+                Snippet = " ",
+                String = " ",
+                Struct = " ",
+                Text = " ",
+                TypeParameter = " ",
+                Unit = " ",
+                Value = " ",
+                Variable = " ",
+            },
+        })
+
+        local opts = {
+            keymap = {
+                preset = 'default',
+                ['<CR>'] = { 'select_and_accept', 'fallback' }
+            },
+            appearance = {
+                nerd_font_variant = 'mono'
+            },
+            completion = {
+                documentation = {
+                    auto_show = true
+                },
+                menu = {
+                    draw = {
+                        components = {
+                            kind_icon = {
+                                text = function(item)
+                                    local kind = require("lspkind").symbol_map[item.kind] or ""
+                                    return kind .. ""
+                                end
+                            }
+                        }
+                    }
+                }
+            },
+            sources = {
+                default = { 'lsp', 'path', 'buffer', 'snippets' },
+            },
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+        }
+
+        require('blink.cmp').setup(opts)
+    end,
 }
