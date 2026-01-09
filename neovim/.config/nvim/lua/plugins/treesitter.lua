b3d1dffb12e735bfeb8a598e3ec4aa37501ea99b
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-01-09 13:19:06 -0500

    [nvim] update treesitter to new version

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,48 +1,54 @@
 return {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-        require('nvim-treesitter.configs').setup({
-            ensure_installed = {
-                'bash',
-                'c',
-                'cmake',
-                'cpp',
-                'css',
-                'diff',
-                'gitignore',
-                'glimmer',
-                'go',
-                'graphql',
-                'hcl',
-                'html',
-                'http',
-                'javascript',
-                'jsdoc',
-                'json',
-                'jsonc',
-                'lua',
-                'markdown',
-                'markdown_inline',
-                'python',
-                'query',
-                'regex',
-                'rust',
-                'scss',
-                'sql',
-                'terraform',
-                'toml',
-                'tsx',
-                'typescript',
-                'vim',
-                'yaml',
-            },
-            highlight = { enable = true },
-            query_linter = {
-                enable = true,
-                use_virtual_text = true,
-                lint_events = { 'BufWrite', 'CursorHold' },
-            },
-            additional_vim_regex_highlighting = false,
+        local ts = require('nvim-treesitter')
+
+        local parsers = {
+            'bash',
+            'c',
+            'cmake',
+            'cpp',
+            'css',
+            'diff',
+            'gitignore',
+            'glimmer',
+            'go',
+            'graphql',
+            'hcl',
+            'html',
+            'http',
+            'javascript',
+            'jsdoc',
+            'json',
+            'jsonc',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'python',
+            'query',
+            'regex',
+            'rust',
+            'scss',
+            'sql',
+            'terraform',
+            'toml',
+            'tsx',
+            'typescript',
+            'vim',
+            'yaml',
+        }
+
+        for _, parser in ipairs(parsers) do
+            pcall(ts.install, parser)
+        end
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end
         })
     end,
 }
