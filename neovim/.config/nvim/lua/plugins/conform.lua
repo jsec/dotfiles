58e40b0f0c022e2fadd23eed723ab1c2118ed714
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-03-03 11:20:25 -0500

    [nvim] stylua

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -1,49 +1,49 @@
 return {
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    opts = {
-        log_level = vim.log.levels.DEBUG,
-        formatters_by_ft = {
-            javascript = {
-                'biome',
-                'biome-organize-imports',
-                'eslint'
-            },
-            typescript = {
-                'biome',
-                'biome-organize-imports',
-                'eslint'
-            },
-            python = { 'isort', 'black' },
-            go = { 'gofmt', 'goimports' },
-            sql = { 'sqlfluff' },
-        },
-        formatters = {
-            biome = {
-                condition = function(_, ctx)
-                    return vim.fs.find({ 'biome.json' }, { path = ctx.filename, upward = true })[1]
-                end,
-            },
-            ['biome-organize-imports'] = {
-                condition = function(_, ctx)
-                    return vim.fs.find({ 'biome.json' }, { path = ctx.filename, upward = true })[1]
-                end,
-            },
-            eslint = {
-                condition = function(_, ctx)
-                    return vim.fs.find({ 'eslint.config.js' }, { path = ctx.filename, upward = true })[1]
-                end
-            },
-            sqlfluff = {
-                inherit = false,
-                command = 'sqlfluff',
-                args = { 'fix', '--config', '/Users/jarrod.seccombe/.sqlfluff', '-' },
-            },
-        },
-        format_on_save = {
-            timeout_ms = 1000,
-            lsp_fallback = true,
-        },
-    },
+	'stevearc/conform.nvim',
+	event = { 'BufWritePre' },
+	cmd = { 'ConformInfo' },
+	opts = {
+		log_level = vim.log.levels.DEBUG,
+		formatters_by_ft = {
+			javascript = {
+				'biome',
+				'biome-organize-imports',
+				'eslint'
+			},
+			typescript = {
+				'biome',
+				'biome-organize-imports',
+				'eslint'
+			},
+			python = { 'isort', 'black' },
+			go = { 'gofmt', 'goimports' },
+			sql = { 'sqlfluff' },
+		},
+		formatters = {
+			biome = {
+				condition = function(_, ctx)
+					return vim.fs.find({ 'biome.json' }, { path = ctx.filename, upward = true })[1]
+				end,
+			},
+			['biome-organize-imports'] = {
+				condition = function(_, ctx)
+					return vim.fs.find({ 'biome.json' }, { path = ctx.filename, upward = true })[1]
+				end,
+			},
+			eslint = {
+				condition = function(_, ctx)
+					return vim.fs.find({ 'eslint.config.js' }, { path = ctx.filename, upward = true })[1]
+				end
+			},
+			sqlfluff = {
+				inherit = false,
+				command = 'sqlfluff',
+				args = { 'fix', '--config', '/Users/jarrod.seccombe/.sqlfluff', '-' },
+			},
+		},
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_fallback = true,
+		},
+	},
 }
