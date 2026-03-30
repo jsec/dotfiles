0312fb6b464c684d086509e780411bbda892a734
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-03-30 12:24:05 -0400

    [nvim] disable semantic tokens for terraform-ls

--- neovim/.config/nvim/lsp/terraformls.lua
+++ neovim/.config/nvim/lsp/terraformls.lua
@@ -1,12 +1,29 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { 'terraform-ls', 'serve', '-log-file', '/dev/null' },
-    filetypes = {
-        'terraform',
-        'terraform-vars',
-    },
-    root_markers = {
-        '.terraform',
-        '.git',
-    },
+	cmd = { 'terraform-ls', 'serve', '-log-file', '/dev/null' },
+	filetypes = {
+		'terraform',
+		'terraform-vars',
+	},
+	root_dir = function(bufnr, on_dir)
+		local path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr))
+
+		local root_file = vim.fs.find({
+			'.terraform',
+			'.terraform.lock.hcl'
+		}, {
+			path = path,
+			upward = true
+		})[1]
+
+		if root_file then
+			on_dir(vim.fs.dirname(root_file))
+			return
+		end
+
+		on_dir(path)
+	end,
+	on_attach = function(client)
+		client.server_capabilities.semanticTokensProvider = nil
+	end
 }
--- neovim/.config/nvim/lsp/tflint.lua
+++ neovim/.config/nvim/lsp/tflint.lua
@@ -1,9 +1,24 @@
 ---@type vim.lsp.Config
 return {
-    cmd = { 'tflint', '--langserver' },
-    filetypes = { 'terraform' },
-    root_markers = {
-        '.terraform',
-        '.git',
-    },
+	cmd = { 'tflint', '--langserver' },
+	filetypes = { 'terraform' },
+	root_dir = function(bufnr, on_dir)
+		local path = vim.fs.dirname(vim.api.nvim_buf_get_name(bufnr))
+
+		local root_file = vim.fs.find({
+			'.tflint.hcl',
+			'.terraform',
+			'.terraform.lock.hcl'
+		}, {
+			path = path,
+			upward = true
+		})[1]
+
+		if root_file then
+			on_dir(vim.fs.dirname(root_file))
+			return
+		end
+
+		on_dir(path)
+	end
 }
