4db63dd1239701b555a5f49f50d6444067ad7955
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-06-22 15:01:32 -0400

    [nvim] update treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,50 +1,54 @@
+local parsers = {
+    'bash',
+    'c',
+    'cmake',
+    'cpp',
+    'css',
+    'diff',
+    'gitignore',
+    'go',
+    'graphql',
+    'html',
+    'http',
+    'javascript',
+    'jsdoc',
+    'json',
+    'lua',
+    'markdown',
+    'markdown_inline',
+    'python',
+    'query',
+    'regex',
+    'rust',
+    'scss',
+    'sql',
+    'toml',
+    'tsx',
+    'typescript',
+    'typespec',
+    'vim',
+    'vue',
+    'yaml',
+}
+
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        config = function()
-            require('nvim-treesitter.configs').setup({
-                ensure_installed = {
-                    'bash',
-                    'c',
-                    'cmake',
-                    'cpp',
-                    'css',
-                    'diff',
-                    'gitignore',
-                    'go',
-                    'graphql',
-                    'help',
-                    'html',
-                    'http',
-                    'javascript',
-                    'jsdoc',
-                    'json',
-                    'jsonc',
-                    'lua',
-                    'markdown',
-                    'markdown_inline',
-                    'python',
-                    'query',
-                    'regex',
-                    'rust',
-                    'scss',
-                    'sql',
-                    'toml',
-                    'tsx',
-                    'typescript',
-                    'typespec',
-                    'vim',
-                    'vue',
-                    'yaml',
-                },
-                highlight = { enable = true },
-                query_linter = {
-                    enable = true,
-                    use_virtual_text = true,
-                    lint_events = { 'BufWrite', 'CursorHold' },
-                },
-                additional_vim_regex_highlighting = false,
-            })
-        end,
-    },
+    'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
+    config = function()
+        local ts = require('nvim-treesitter')
+
+        ts.setup({
+            highlight = { enable = true },
+            query_linter = {
+                enable = true,
+                use_virtual_text = true,
+                lint_events = { 'BufWrite', 'CursorHold' },
+            },
+            additional_vim_regex_highlighting = false,
+        })
+
+        ts.install(parsers):wait(300000)
+    end,
 }
