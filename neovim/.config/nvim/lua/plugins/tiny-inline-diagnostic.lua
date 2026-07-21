54fd76c277f350e3882e596143a7ed451c6d1c9d
Jarrod Seccombe <jarrod.seccombe@icloud.com>
2026-07-21 18:37:55 -0400

    [nvim] better debouncing for tiny-inline-diagnostic

--- neovim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ neovim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -5,7 +5,15 @@ return {
         },
     },
     setup = function()
-        require('tiny-inline-diagnostic').setup()
+        require('tiny-inline-diagnostic').setup({
+            options = {
+                throttle = 60,
+                enable_on_insert = false,
+                multilines = { enabled = false },
+                show_all_diags_on_cursorline = false,
+            }
+        })
+
         vim.diagnostic.config({ virtual_text = false })
     end,
 }
